c58a20392f857b500791f3d8a048e157db98dcf8
firreee <143442624+firreee@users.noreply.github.com>
2024-03-20 10:45:36 -0500

    Update P15.py

--- P15.py
+++ P15.py
@@ -51,9 +51,4 @@ dev_2 = Developer('Test', 'Employee', 60000, 'Java')
 
 mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
 
-print(mgr_1.email)
-
-mgr_1.add_emp(dev_2)
-mgr_1.remove_emp(dev_2)
-
-mgr_1.print_emps()
+print(issubclass(Manager, Developer))
